9ab3cca0490d883c85c99b674769841f71f5a489
matheussantana2007 <matheus.santana114@etec.sp.gov.br>
2024-04-26 23:07:23 -0300

    feat: classes to explain data types in Java.

---
+++ src/defaultpackage/Data_Type01.java
@@ -0,0 +1,19 @@
+// Tipos de Números
+// Numéricos (Intereiros e Reais)
+
+package defaultpackage;
+
+public class Data_Type01 {
+	// Tipos primitivos
+	public static void main(String[] args) {
+		int num1 = 4; // Inteiro
+		short num2 = 4; // Inteiro (curto/menor/baixo)
+		
+		// Tipos não primitivos/primários
+		// Classes dos tipos de números
+		Integer num3 = 7;
+		Short num4 = 7;
+
+	}
+
+}
---
+++ src/defaultpackage/Data_Type02.java
@@ -0,0 +1,21 @@
+// Tipos de Números
+// Numéricos (Intereiros e Reais)
+
+package defaultpackage;
+
+public class Data_Type02 {
+
+	public static void main(String[] args) {
+		// Tipos primários
+		// Por padrão números com decimais são reconhecidos como double
+		float num1 = 25.4f; // float ocupa menos espaço de memória
+		double num2 = 25.4;
+		
+		// Tipos não primários/primitivos
+		// Classes dos tipos de números
+		// Ocupa mais espaço memória
+		Float num3 = 25.4f;
+		Double num4 = 25.4;
+	}
+
+}
