389437050cf0a13ab42c93a971e7eded5bec2a0d
matheussantana2007 <matheus.santana114@etec.sp.gov.br>
2024-04-26 23:30:07 -0300

    feat: display of variable values

--- src/defaultpackage/Data_Type01.java
+++ src/defaultpackage/Data_Type01.java
@@ -14,7 +14,14 @@ public class Data_Type01 {
 		// Classes dos tipos de números
 		Integer num4 = 7;
 		Short num5 = 8;
-
+		Byte num6 = 10;
+		
+		System.out.println("int -> Num1: " + num1);
+		System.out.println("short -> Num2: " + num2);
+		System.out.println("byte -> Num3: " + num3);
+		System.out.println("Integer -> Num4: " + num4);
+		System.out.println("Short -> Num5: " + num5);
+		System.out.println("Byte -> Num6: " + num6);
 	}
 
 }
