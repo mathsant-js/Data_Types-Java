2cad78d341d7934029118c8baddc09a720dba560
matheussantana2007 <matheus.santana114@etec.sp.gov.br>
2024-05-01 12:35:06 -0300

    feat: basic math operations have been added

---
+++ src/defaultpackage/Operacoes_metematicas.java
@@ -0,0 +1,27 @@
+// Operações matemáticas
+package defaultpackage;
+
+public class Operacoes_metematicas {
+
+	public static void main(String[] args) {
+		// Declaração de variáveis
+		double num1 = 9, num2 = 5, res;
+		
+		// Soma
+		res = num1 + num2;
+		System.out.println(num1 + " + " + num2 + " = " + res);
+		
+		// Subtração
+		res = num1 - num2;
+		System.out.println(num1 + " - " + num2 + " = " + res);
+		
+		// Multiplicação
+		res = num1 * num2;
+		System.out.println(num1 + " * " + num2 + " = " + res);
+		
+		// Divisão
+		res = num1 / num2;
+		System.out.println(num1 + " / " + num2 + " = " + res);
+	}
+
+}
